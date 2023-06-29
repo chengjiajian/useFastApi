41600de4dacfd2e974128d8fde63e745f3abf62f
成佳健 <chengjiajian@shiyugroup.com>
2023-06-29 14:21:35 +0800

    test 2

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ async def read_root():
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/items/{item_id}")
+async def read_item1(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
-#test
+
